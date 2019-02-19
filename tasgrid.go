f93320c8a6cbc20d4b9569a5862b6ce1adb89414
Miguel de Salas <Miguel.deSalas@tmag.tas.gov.au>
2019-02-19 16:43:16 +1100

    Account for unit wrap in coords

--- tasgrid.go
+++ tasgrid.go
@@ -8,20 +8,80 @@ import (
 	"strconv"
 )
 
-// TasMap holds map-unique information
-type TasMap struct {
-	zone          int
-	alpha         string
-	eastingStart  int
-	northingStart int
-}
-
 // MapGrid holds a dictionary of TASMAP three-letter acronyms
 // containing the map's necessary data to calculate the full grid reference
 type MapGrid map[string]TasMap
 
+// TasMap holds map-unique information
+type TasMap struct {
+	zone          int
+	alpha         string
+	eastingStart  string
+	northingStart string
+}
+
+func NewTasMap(mapInfo []string) TasMap {
+	zone, err := strconv.Atoi(mapInfo[1])
+	checkError(err)
+	alpha := mapInfo[2]
+	east := mapInfo[3]
+	north := mapInfo[4]
+
+	return TasMap{zone: zone, alpha: alpha, eastingStart: east, northingStart: north}
+}
+
+// GridPoint holds all the necessary information to calculate the full
+// grid reference of a point from its 6-figure map grid reference
+type GridPoint struct {
+	mapName      string
+	easting3d    string
+	northing3d   string
+	fullEasting  string
+	fullNorthing string
+}
+
+// NewGridPoint creates a GridPoint from the supplied map name and 3-digit
+// easting and northing
+func NewGridPoint(name, eas, nor string, mg MapGrid) (gp GridPoint) {
+	gp = GridPoint{mapName: name, easting3d: eas, northing3d: nor}
+
+	easting, err := strconv.Atoi(eas)
+	checkError(err)
+	northing, err := strconv.Atoi(nor)
+	checkError(err)
+
+	ess := mg[name].eastingStart
+	nss := mg[name].northingStart
+
+	easStart, err := strconv.Atoi(ess[:1])
+	norStart, err := strconv.Atoi(nss[:2])
+
+	easEnd, err := strconv.Atoi(ess[len(ess)-2:])
+	checkError(err)
+	norEnd, err := strconv.Atoi(nss[len(nss)-2:])
+	checkError(err)
+
+	if easting > easEnd*10 {
+		gp.fullEasting = ess[:1] + eas + "00"
+	} else {
+		newEss := strconv.Itoa(easStart + 1)
+		gp.fullEasting = newEss + eas + "00"
+	}
+
+	if northing > norEnd*10 {
+		gp.fullNorthing = nss[:2] + nor + "00"
+	} else {
+		newNss := strconv.Itoa(norStart + 1)
+		gp.fullNorthing = newNss + nor + "00"
+	}
+
+	fmt.Println(eas, easEnd*10, easStart, nor, norEnd*10, norStart, gp.fullEasting, gp.fullNorthing)
+
+	return gp
+}
+
 // NewTasMapGrid returns a TasMap object
-func NewTasMapGrid() *MapGrid {
+func NewTasMapGrid() MapGrid {
 	mapList := MapGrid{}
 
 	mapFile, err := os.Open("../mypacks/tasgrid/mapinfo.csv")
@@ -40,16 +100,10 @@ func NewTasMapGrid() *MapGrid {
 		}
 
 		name := tasMap[0]
-		zone, err := strconv.Atoi(tasMap[1])
-		alpha := tasMap[2]
-		east, err := strconv.Atoi(tasMap[3])
-		north, err := strconv.Atoi(tasMap[4])
-
-		mapList[name] = TasMap{zone: zone, alpha: alpha, eastingStart: east, northingStart: north}
-
+		mapList[name] = NewTasMap(tasMap)
 	}
 
-	return &mapList
+	return mapList
 }
 
 func checkError(err error) {
