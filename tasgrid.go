85f7781d1901dcc36d2b22ba252a0355130e12f8
kurankat <Miguel.deSalas@tmag.tas.gov.au>
2019-02-21 12:03:41 +1100

    Add error return and extra coordinates

--- tasgrid.go
+++ tasgrid.go
@@ -4,8 +4,11 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"math"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	utm "github.com/kurankat/UTM"
 )
@@ -32,37 +35,59 @@ func NewTasMap(mapInfo []string) TasMap {
 // GridPoint holds all the necessary information to calculate the full
 // grid reference of a point from its 6-figure map grid reference
 type GridPoint struct {
-	mapName      string
+	MapName      string
 	easting3d    string
 	northing3d   string
 	fullEasting  float64
 	fullNorthing float64
 	decimalLat   float64
 	decimalLong  float64
+	latDegs      float64
+	latMins      float64
+	latSecs      float64
+	longDegs     float64
+	longMins     float64
+	longSecs     float64
 }
 
 // NewGridPoint creates a GridPoint from the supplied map name and 3-digit
 // easting and northing
-func NewGridPoint(name, eas, nor string, mg MapGrid) (gp GridPoint) {
-	gp = GridPoint{mapName: name, easting3d: eas, northing3d: nor}
+func NewGridPoint(name, eas, nor string, mg MapGrid) (GridPoint, error) {
+	if len(name) != 3 || len(eas) != 3 || len(nor) != 3 {
+		return GridPoint{}, nil
+	}
+	mapName := strings.ToUpper(name)
+	gp := GridPoint{MapName: mapName, easting3d: eas, northing3d: nor}
 	var strEasting string
 	var strNorthing string
 
 	easting, err := strconv.Atoi(eas)
-	checkError(err)
+	if err != nil {
+		return GridPoint{}, fmt.Errorf("ERROR parsing grid: I can't convert easting %v to an integer", eas)
+	}
 	northing, err := strconv.Atoi(nor)
-	checkError(err)
+	if err != nil {
+		return GridPoint{}, fmt.Errorf("ERROR parsing grid: I can't convert northing %v to an integer", nor)
+	}
 
-	ess := mg[name].eastingStart
-	nss := mg[name].northingStart
+	ess := mg[mapName].eastingStart
+	nss := mg[mapName].northingStart
+
+	if len(ess)+len(nss) == 0 {
+		return GridPoint{}, fmt.Errorf("ERROR parsing grid: I'm having trouble geting values for map %v", name)
+	}
 
 	easStart, err := strconv.Atoi(ess[:1])
 	norStart, err := strconv.Atoi(nss[:2])
 
 	easEnd, err := strconv.Atoi(ess[len(ess)-2:])
-	checkError(err)
+	if err != nil {
+		return GridPoint{}, fmt.Errorf("ERROR parsing grid: I can't extract a number from %v", ess)
+	}
 	norEnd, err := strconv.Atoi(nss[len(nss)-2:])
-	checkError(err)
+	if err != nil {
+		return GridPoint{}, fmt.Errorf("ERROR parsing grid: I can't extract a number from %v", nss)
+	}
 
 	if easting > easEnd*10 {
 		strEasting = ess[:1] + eas + "00"
@@ -81,10 +106,15 @@ func NewGridPoint(name, eas, nor string, mg MapGrid) (gp GridPoint) {
 	gp.fullNorthing, err = strconv.ParseFloat(strNorthing, 64)
 
 	gp.decimalLat, gp.decimalLong, err = utm.ToLatLon(gp.fullEasting, gp.fullNorthing, 55, "G")
+	if err != nil {
+		fmt.Printf("Map name: %v, 3f easting: %v, 3f northing: %v", mapName, eas, nor)
+		panic(err)
+	}
 
-	fmt.Println(eas, easEnd*10, easStart, nor, norEnd*10, norStart, gp.fullEasting, gp.fullNorthing)
+	gp.latDegs, gp.latMins, gp.latSecs = ddToDMS(gp.decimalLat)
+	gp.longDegs, gp.longMins, gp.longSecs = ddToDMS(gp.decimalLong)
 
-	return gp
+	return gp, nil
 }
 
 // GetFullEasting returns the full-length easting of the grid point
@@ -96,19 +126,37 @@ func (gp GridPoint) GetFullEasting() (easting string) {
 // GetFullNorthing returns the full-length northing of the grid point
 func (gp GridPoint) GetFullNorthing() (northing string) {
 	northing = strconv.FormatFloat(gp.fullNorthing, 'f', 0, 64)
-	return northing
+	return
 }
 
 // GetDecimalLat returns the latitude in decimal degrees of the grid point
 func (gp GridPoint) GetDecimalLat() (dLat string) {
 	dLat = strconv.FormatFloat(gp.decimalLat, 'f', 6, 64)
-	return dLat
+	return
 }
 
 // GetDecimalLong returns the longitude in decimal degrees of the grid point
 func (gp GridPoint) GetDecimalLong() (dLong string) {
 	dLong = strconv.FormatFloat(gp.decimalLong, 'f', 6, 64)
-	return dLong
+	return
+}
+
+// GetLatSeconds returns the seconds reading of the grid point's latitude
+func (gp GridPoint) GetLatSeconds() (secs string) {
+	if gp.latSecs < 0 {
+		gp.latSecs = -gp.latSecs
+	}
+	secs = strconv.FormatFloat(gp.latSecs, 'f', 1, 64)
+	return
+}
+
+// GetLongSeconds returns the seconds reading of the grid point's latitude
+func (gp GridPoint) GetLongSeconds() (secs string) {
+	if gp.longSecs < 0 {
+		gp.longSecs = -gp.longSecs
+	}
+	secs = strconv.FormatFloat(gp.longSecs, 'f', 1, 64)
+	return
 }
 
 // MapGrid holds a dictionary of TASMAP three-letter acronyms
@@ -119,7 +167,8 @@ type MapGrid map[string]TasMap
 func NewTasMapGrid() MapGrid {
 	mapList := MapGrid{}
 
-	mapFile, err := os.Open("../mypacks/tasgrid/mapinfo.csv")
+	gopath := os.Getenv("GOPATH")
+	mapFile, err := os.Open(filepath.Join(gopath, "src/mypacks/tasgrid/mapinfo.csv"))
 	checkError(err)
 	defer mapFile.Close() // Defer closing until the program is done
 
@@ -141,9 +190,24 @@ func NewTasMapGrid() MapGrid {
 	return mapList
 }
 
+func ddToDMS(dd float64) (degs, mins, secs float64) {
+
+	degs = math.Trunc(dd)                 // Degrees as float is the truncated decimal degrees
+	dMin := math.Abs(dd) - math.Abs(degs) // What remains right of the decimal point is the decimal mins
+	tempMins := dMin * 60.0               // Float minutes is decimal minutes * 60
+	mins = math.Trunc(tempMins)
+	dSecs := tempMins - mins
+	secs = dSecs * 60.0
+
+	// degs = strconv.FormatFloat(fDegs, 'f', 0, 64)
+	// mins = strconv.FormatFloat(fMins, 'f', 0, 64)
+	// secs = strconv.FormatFloat(fSecs, 'f', 1, 64)
+
+	return
+}
+
 func checkError(err error) {
 	if err != nil {
-		// fmt.Printf("Error type: %T\n", err)
 		switch err.(type) {
 		// If the error is a path error (such as file not being found) then print
 		// individualised error message
